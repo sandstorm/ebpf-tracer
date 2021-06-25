307069ebc34948c20cbdba373034bd17d8641709
Sebastian Kurfürst <sebastian.kurfuerst@sandstorm.de>
2021-06-25 09:57:23 +0200

    !!!remove experiments, as they are now part of the git history!!!

--- legacy-netshoot-based-alpine/entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-set -e
-
-mount -t debugfs debugfs /sys/kernel/debug
-
-chmod +x /usr/share/bpftrace/tools/*
-
-export PATH=$PATH:/usr/share/bpftrace/tools/:/usr/share/bcc/tools/
-exec zsh
